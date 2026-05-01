d7c86fb320424b011ec212df7285d0bc48fbb583
rumina-v <viktoriarumina510@gmail.com>
2026-05-01 14:57:48 +0300

    Update User.cs

--- DetectiveInterrogation/Models/Entities/User.cs
+++ DetectiveInterrogation/Models/Entities/User.cs
@@ -3,14 +3,17 @@ namespace DetectiveInterrogation.Models.Entities;
 public class User
 {
     public int Id { get; set; }
-    
+
     public string Username { get; set; } = string.Empty;
-    
+
     public string Email { get; set; } = string.Empty;
-    
+
     public string PasswordHash { get; set; } = string.Empty;
+
+    public string Role { get; set; } = "User";
 
     // Navigation properties
     public ICollection<InterrogationSession> InterrogationSessions { get; set; } = new List<InterrogationSession>();
+
     public ICollection<UserAchievement> UserAchievements { get; set; } = new List<UserAchievement>();
 }
