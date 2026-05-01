2ebbcc3aa816a0c789e579c2b8ffb09c6b2b3745
rumina-v <viktoriarumina510@gmail.com>
2026-05-01 14:55:45 +0300

    Add check constraints for CurrentTrust and CurrentAggression
    Added check constraints for CurrentTrust and CurrentAggression properties to ensure values are within the range of 0 to 100.

--- DetectiveInterrogation/Data/Configurations/InterrogationSessionConfiguration.cs
+++ DetectiveInterrogation/Data/Configurations/InterrogationSessionConfiguration.cs
@@ -9,35 +9,55 @@ public class InterrogationSessionConfiguration : IEntityTypeConfiguration<Interr
     public void Configure(EntityTypeBuilder<InterrogationSession> builder)
     {
         builder.HasKey(s => s.Id);
-        
+
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint(
+                "CK_InterrogationSessions_CurrentTrust_Range",
+                "[CurrentTrust] BETWEEN 0 AND 100");
+
+            t.HasCheckConstraint(
+                "CK_InterrogationSessions_CurrentAggression_Range",
+                "[CurrentAggression] BETWEEN 0 AND 100");
+        });
+
         builder.Property(s => s.CaseId)
             .IsRequired();
-        
+
         builder.Property(s => s.SuspectId)
             .IsRequired();
-        
+
         builder.Property(s => s.UserId)
             .IsRequired();
-        
+
         builder.Property(s => s.CurrentTrust)
+            .IsRequired()
             .HasDefaultValue(50);
-        
+
         builder.Property(s => s.CurrentAggression)
+            .IsRequired()
             .HasDefaultValue(50);
-        
-        // Check constraints for trust and aggression
-        builder.Property(s => s.CurrentTrust).HasConversion(new Microsoft.EntityFrameworkCore.Storage.ValueConverter<int, int>(
-            v => v > 100 ? 100 : (v < 0 ? 0 : v),
-            v => v));
-        
-        builder.Property(s => s.CurrentAggression).HasConversion(new Microsoft.EntityFrameworkCore.Storage.ValueConverter<int, int>(
-            v => v > 100 ? 100 : (v < 0 ? 0 : v),
-            v => v));
-        
+
         builder.Property(s => s.Status)
+            .IsRequired()
             .HasDefaultValue("InProgress")
             .HasMaxLength(20);
-        
+
+        builder.HasOne(s => s.Case)
+            .WithMany()
+            .HasForeignKey(s => s.CaseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(s => s.Suspect)
+            .WithMany(s => s.InterrogationSessions)
+            .HasForeignKey(s => s.SuspectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(s => s.User)
+            .WithMany(u => u.InterrogationSessions)
+            .HasForeignKey(s => s.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
         builder.HasMany(s => s.SessionUsedEvidences)
             .WithOne(sue => sue.Session)
             .HasForeignKey(sue => sue.SessionId)
