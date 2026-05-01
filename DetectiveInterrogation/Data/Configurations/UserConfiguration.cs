7388a9f839a6ce1cec936c67366dbf41d5fffb63
rumina-v <viktoriarumina510@gmail.com>
2026-05-01 14:58:27 +0300

    Add check constraint for User role values

--- DetectiveInterrogation/Data/Configurations/UserConfiguration.cs
+++ DetectiveInterrogation/Data/Configurations/UserConfiguration.cs
@@ -9,26 +9,41 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(u => u.Id);
-        
+
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint(
+                "CK_Users_Role_AllowedValues",
+                "[Role] IN ('User', 'Admin')");
+        });
+
         builder.Property(u => u.Username)
             .IsRequired()
             .HasMaxLength(50);
-        
+
         builder.Property(u => u.Email)
             .IsRequired()
             .HasMaxLength(255);
-        
+
         builder.Property(u => u.PasswordHash)
             .IsRequired();
-        
-        builder.HasIndex(u => u.Username).IsUnique();
-        builder.HasIndex(u => u.Email).IsUnique();
-        
+
+        builder.Property(u => u.Role)
+            .IsRequired()
+            .HasMaxLength(20)
+            .HasDefaultValue("User");
+
+        builder.HasIndex(u => u.Username)
+            .IsUnique();
+
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
         builder.HasMany(u => u.InterrogationSessions)
             .WithOne(s => s.User)
             .HasForeignKey(s => s.UserId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
         builder.HasMany(u => u.UserAchievements)
             .WithOne(ua => ua.User)
             .HasForeignKey(ua => ua.UserId)
